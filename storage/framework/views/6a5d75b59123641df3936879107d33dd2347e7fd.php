05b0ddf1fe88bdfe5459aad32966f75b9e1bdd15
Isroilov Firdavs <firdavsisroilov28@gmail.com>
2025-09-14 21:03:29 +0500

    first commit

---
+++ storage/framework/views/6a5d75b59123641df3936879107d33dd2347e7fd.php
@@ -0,0 +1,5 @@
+
+<body>
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH /Users/macbookpro/Desktop/laravel_dashboard/velzon/Velzon_Laravel_v1.3.0/material/resources/views/layouts/body.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8998e8ad121804343748be122361767003d6bdcd.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Velzon.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Design & Develop by Themesbrand
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php /**PATH /Users/macbookpro/Desktop/laravel_dashboard/velzon/Velzon_Laravel_v1.3.0/material/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
